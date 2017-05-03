be9af1043f45fa9d8fdd4f63f78be6a96c94522e
giselle1073 <giselle.salgado@speeracademy.org>
2017-05-03 17:30:52 -0500

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,11 @@
+var addTask = function() {
+
+var task = document.getElementById("inputText1").value;
+localStorage.setItem(0,task);
+
+
+var savedTask = localStorage.getItem(0);
+var establishedList = document.getElementByTag 
+
+
+	}
